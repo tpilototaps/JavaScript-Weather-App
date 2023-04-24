de47fa5c098220fe1166cf7009735d6330535c62
tpilototaps <tpiloto@liberty.edu>
2023-04-24 18:42:33 -0400

    improved UX

--- main.js
+++ main.js
@@ -1,62 +1,32 @@
 
 function your_weather(){
     let location = document.getElementById("input-here").value;
-    console.log(location)
-
-
-    
+   
     const access = async () => {
-
-
 
         const responseloc = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=1d2ff8f43a0360e77507edf6bae62d96`)
         const dataloc = await responseloc.json()
-        console.log(dataloc)
         const geoloclat = dataloc[0]['lat']
         const geoloclon = dataloc[0]['lon']
-        console.log(geoloclat)
-        console.log(geoloclon)
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoloclat}&lon=${geoloclon}&appid=1d2ff8f43a0360e77507edf6bae62d96`)
         const data = await response.json()
-        console.log(data)
         
-        const name = document.createElement('h1')
-        name.innerText = data['city']['name']
-        const city = document.querySelector('.city')
-        city.append(name)
+        document.querySelector('.city').innerText ='Weather In' + ' ' + data['city']['name'] + ':'
 
         let digitemp = data['list'][0]['main']['temp_max']
-        console.log(digitemp)
-        let fahrenheitHigh = ((digitemp - 273.15)*1.8) + 32
-        console.log(fahrenheitHigh)
-        const high = document.createElement('h4')
-        high.innerText = fahrenheitHigh
-        console.log(high)
-        const highest_temp = document.querySelector('.highest-temp')
-        highest_temp.append(high)
+        let fahrenheitHigh = Math.round(((digitemp - 273.15)*1.8) + 32)
+        document.querySelector('.highest-temp').innerText = fahrenheitHigh    
 
         let digitempl = data['list'][0]['main']['temp_min']
-        console.log(digitempl)
-        let fahrenheitlow = ((digitempl - 273.15)*1.8) + 32
-        console.log(fahrenheitlow)
-        const low = document.createElement('h4')
-        low.innerText = fahrenheitlow
-        console.log(low)
-        const lowest_temp = document.querySelector('.lowest-temp')
-        lowest_temp.append(low)
+        let fahrenheitlow = Math.round(((digitempl - 273.15)*1.8) + 32)
+        document.querySelector('.lowest-temp').innerText = fahrenheitlow
+        
+        document.querySelector('.coverage').innerText = data['list'][0]['clouds']['all']    
 
-        const clouds = document.createElement('h4')
-        clouds.innerText = data['list'][0]['clouds']['all']
-        const coverage = document.querySelector('.coverage')
-        coverage.append(clouds)
+        document.querySelector('.relative-humidity').innerText = data['list'][0]['main']['humidity']
 
-        const relative_humidity = document.createElement('h4')
-        relative_humidity.innerText = data['list'][0]['main']['humidity']
-        const humidity = document.querySelector('.relative-humidity')
-        humidity.append(relative_humidity)
     }
     
-
     access()
 }
